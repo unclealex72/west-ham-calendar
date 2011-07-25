044c411bcacb2629ef8645d71af89a70c12c8690
Alex Jones <alex.jones@unclealex.co.uk>
2011-07-25 21:41:04 +0000

    Updating to use GWT.

---
+++ src/main/java/uk/co/unclealex/hammers/calendar/server/servlet/AdminAttendanceServlet.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package uk.co.unclealex.hammers.calendar.server.servlet;
+
+import java.io.IOException;
+
+import uk.co.unclealex.hammers.calendar.shared.exceptions.GoogleAuthenticationFailedException;
+import uk.co.unclealex.hammers.calendar.shared.exceptions.GoogleException;
+import uk.co.unclealex.hammers.calendar.shared.model.CalendarColour;
+import uk.co.unclealex.hammers.calendar.shared.model.CalendarConfiguration;
+import uk.co.unclealex.hammers.calendar.shared.model.CalendarType;
+import uk.co.unclealex.hammers.calendar.shared.remote.AdminAttendanceService;
+
+/**
+ * Copyright 2011 Alex Jones
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ *
+ * @author unclealex72
+ *
+ */
+public class AdminAttendanceServlet extends AbstractAttendanceServlet implements AdminAttendanceService {
+
+	@Override
+	public void remove(CalendarType calendarType) throws GoogleAuthenticationFailedException, IOException, GoogleException {
+		createAttendanceService().remove(calendarType);
+	}
+
+	@Override
+	public void createOrUpdate(CalendarConfiguration calendarConfiguration) throws GoogleAuthenticationFailedException,
+			IOException, GoogleException {
+		createAttendanceService().createOrUpdate(calendarConfiguration);
+
+	}
+
+	@Override
+	public CalendarConfiguration[] getAllCalendarConfigurations() {
+		return createAttendanceService().getAllCalendarConfigurations();
+	}
+
+	@Override
+	public void authenticate(String successToken) throws GoogleAuthenticationFailedException, IOException {
+		createAttendanceService().authenticate(successToken);
+	}
+
+	@Override
+	public String createGoogleAuthenticationUrlIfRequired() {
+		return createAttendanceService().createGoogleAuthenticationUrlIfRequired();
+	}
+	
+	@Override
+	public CalendarColour[] getUsedCalendarColours() throws GoogleAuthenticationFailedException, IOException, GoogleException {
+		return createAttendanceService().getUsedCalendarColours();
+	}
+	
+	@Override
+	public CalendarConfiguration createNewCalendarConfiguration(CalendarType calendarType)
+			throws GoogleAuthenticationFailedException, IOException, GoogleException {
+		return createAttendanceService().createNewCalendarConfiguration(calendarType);
+	}
+	
+	@Override
+	public CalendarConfiguration[] getCalendarConfigurations(boolean tickets) {
+		return createAttendanceService().getCalendarConfigurations(tickets);
+	}
+	
+	@Override
+	public void updateCalendars() {
+		createAttendanceService().updateCalendars();
+		
+	}
+}
---
+++ src/main/java/uk/co/unclealex/hammers/calendar/server/servlet/UserAttendanceServlet.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package uk.co.unclealex.hammers.calendar.server.servlet;
+
+import java.io.IOException;
+
+import uk.co.unclealex.hammers.calendar.shared.exceptions.GoogleAuthenticationFailedException;
+import uk.co.unclealex.hammers.calendar.shared.exceptions.GoogleException;
+import uk.co.unclealex.hammers.calendar.shared.model.Game;
+import uk.co.unclealex.hammers.calendar.shared.remote.UserAttendanceService;
+
+/**
+ * Copyright 2011 Alex Jones
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ *
+ * @author unclealex72
+ *
+ */
+public class UserAttendanceServlet extends AbstractAttendanceServlet implements UserAttendanceService {
+
+	@Override
+	public Game[] attendAllHomeGamesForSeason(int season) {
+		return createAttendanceService().attendAllHomeGamesForSeason(season);
+	}
+
+	@Override
+	public Game attendGame(int gameId) throws GoogleAuthenticationFailedException, IOException, GoogleException {
+		return createAttendanceService().attendGame(gameId);
+	}
+
+	@Override
+	public Game unattendGame(int gameId) throws GoogleAuthenticationFailedException, IOException, GoogleException {
+		return createAttendanceService().unattendGame(gameId);
+	}
+
+	@Override
+	public void forceLogin() {
+		createAttendanceService().forceLogin();
+	}
+
+}
