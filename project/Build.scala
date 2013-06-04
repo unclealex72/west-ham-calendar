653244c5c17d4f64ef63312e262aa5a62379c8b2
Alex Jones <alex.jones@unclealex.co.uk>
2013-06-04 19:09:22 +0100

    Port to Play2 framework mainly, at this point, to allow test reports in Jenkins.

---
+++ project/Build.scala
@@ -0,0 +1,39 @@
+import sbt._
+import Keys._
+import sbt.Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "west-ham-calendar"
+  val appVersion      = "6.0.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+        "com.typesafe"  %% "scalalogging-slf4j" % "1.0.1",
+        "org.mozilla"  % "rhino"  % "1.7R3",
+    "ch.qos.logback" % "logback-classic" % "1.0.6",
+        "javax.transaction"  % "jta"  % "1.1",
+        "joda-time"  % "joda-time"  % "2.2",
+        "org.joda"  % "joda-convert"  % "1.3.1",
+        "net.sourceforge.htmlcleaner"  % "htmlcleaner"  % "2.2",
+        "javax.inject"  % "javax.inject"  % "1",
+    "com.google.inject" % "guice" % "3.0",
+    "com.tzavellas" % "sse-guice" % "0.7.1",
+    "org.squeryl" % "squeryl_2.10.0-RC5" % "0.9.5-5",
+    "org.hsqldb"  % "hsqldb"  % "2.2.8" % "test",
+        "org.specs2"  %% "specs2"  % "1.14"  % "test",
+    jdbc,
+    anorm
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    scalaVersion := "2.10.1",
+    organization := "uk.co.unclealex.calendar",
+    resolvers ++= Seq(
+      "scala-tools" at "https://oss.sonatype.org/content/groups/scala-tools/",
+      "cloudbees-private-release-repository" at "https://repository-unclealex.forge.cloudbees.com/release",
+      "cloudbees-private-snapshot-repository" at "https://repository-unclealex.forge.cloudbees.com/snapshot")    
+  )
+
+}
