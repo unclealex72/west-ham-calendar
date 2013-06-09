f43536924d4c8ed28d9cb7a257ce0b09131ac3f9
Alex Jones <alex.jones@unclealex.co.uk>
2013-06-09 13:59:58 +0100

    Add database creation script.

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,32 @@
+# CallerID schema
+
+# --- !Ups
+
+-- table declarations :
+create table "Game" (
+    "opponents" varchar(128) not null,
+    "attended" boolean,
+    "result" varchar(128),
+    "dateTimePriorityPointPostAvailable" timestamp,
+    "dateTimeSeasonTicketsAvailable" timestamp,
+    "_competition" varchar(128) not null,
+    "season" integer not null,
+    "attendence" integer,
+    "dateTimeAcademyMembersAvailable" timestamp,
+    "id" bigint primary key not null,
+    "_location" varchar(128) not null,
+    "dateTimePlayed" timestamp,
+    "televisionChannel" varchar(128),
+    "matchReport" varchar(128),
+    "dateTimeBondholdersAvailable" timestamp,
+    "dateTimeGeneralSaleAvailable" timestamp
+  );
+create sequence "s_Game_id";
+-- column group indexes :
+create unique index "idxgameKeyComposite" on "Game" ("_competition","_location","opponents","season");
+
+# --- !Downs
+
+DROP INDEX "idxgameKeyComposite";
+DROP TABLE "Game";
+DROP SEQUENCE "s_Game_id";
