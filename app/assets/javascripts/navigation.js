be3c41c89b529393d51ee1c0275ea577b1561635
Alex Jones <alex.jones@unclealex.co.uk>
2014-02-06 21:21:13 +0000

    Add sliders for small screen navigation.

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,4 +1,4 @@
-var app = angular.module('navigation', ['constants', 'ngRoute']);
+var app = angular.module('navigation', ['constants', 'ngRoute', 'ngTouch', 'ngAnimate']);
 
 app.config(['$routeProvider', 'Constants', function($routeProvider, Constants) {
   var defaultSeason = _.max(Constants.seasons);
@@ -78,7 +78,85 @@ app.directive('navigation', ['Navigation', 'Constants', function(Navigation, Con
       $scope.alterTicketType = Navigation.alterTicketType;
       $scope.username = Constants.username;
       $scope.authorised = _.isString(Constants.username);
+
+      $scope.displaySeason = function(season) {
+        return season;
+      };
+      $scope.displayTicketType = function(ticketType) {
+        return ticketType.label + " tickets";
+      };
     }
   };
 }]);
 
+app.directive('slider', [function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      slideSelect: '&',
+      slideDisplay: '&',
+      slides: '='
+    },
+    templateUrl: 'assets/partials/slider.html',
+    link : function($scope, element, attrs) {
+      $scope.direction = 'left';
+      $scope.currentIndex = 0;
+
+      $scope.setCurrentSlideIndex = function (index) {
+        $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+        $scope.currentIndex = index;
+      };
+
+      $scope.isCurrentSlideIndex = function (index) {
+        return $scope.currentIndex === index;
+      };
+
+      $scope.previousSlide = function () {
+        $scope.direction = 'left';
+        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+      };
+
+      $scope.nextSlide = function () {
+        $scope.direction = 'right';
+        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+      };
+    }
+  };
+}]);
+app.animation('.slide-animation', function () {
+    return {
+        addClass: function (element, className, done) {
+            var scope = element.scope();
+
+            if (className == 'ng-hide') {
+                var finishPoint = element.parent().width();
+                if(scope.direction !== 'right') {
+                    finishPoint = -finishPoint;
+                }
+                TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+            }
+            else {
+                done();
+            }
+        },
+        removeClass: function (element, className, done) {
+            var scope = element.scope();
+
+            if (className == 'ng-hide') {
+                element.removeClass('ng-hide');
+
+                var startPoint = element.parent().width();
+                if(scope.direction === 'right') {
+                    startPoint = -startPoint;
+                }
+
+                TweenMax.set(element, { left: startPoint });
+                TweenMax.to(element, 0.5, {left: 0, onComplete: done });
+            }
+            else {
+                done();
+            }
+        }
+    };
+});
