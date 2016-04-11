d0c1e23f4451c6676f383cad2434de41bc920869
Alex Jones <alex.jones@unclealex.co.uk>
2016-04-11 18:22:04 +0100

    Re-add material design icons.

--- copy-javascripts.sh
+++ copy-javascripts.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cat \
-    play/public/bower_components/jquery/dist/jquery.js \
-    play/public/bower_components/angular/angular.js \
-    play/public/bower_components/materialize/dist/js/materialize.js \
+    play/public/bower_components/jquery/dist/jquery.min.js \
+    play/public/bower_components/angular/angular.min.js \
+    play/public/bower_components/materialize/dist/js/materialize.min.js \
     play/public/bower_components/angular-materialize/src/angular-materialize.js \
-    > play/app/assets/javascripts/libs.js
+    > play/public/javascripts/libs.js
