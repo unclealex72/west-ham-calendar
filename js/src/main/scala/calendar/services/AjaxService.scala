6717819e8c6043cb9cad84f122bda2f871da929d
Alex Jones <alex.jones@unclealex.co.uk>
2016-12-18 10:15:27 +0000

    Remove implicit HttpPromise to future call that was no longer working.

--- js/src/main/scala/calendar/services/AjaxService.scala
+++ js/src/main/scala/calendar/services/AjaxService.scala
@@ -1,33 +1,48 @@
 package calendar.services
 
-import com.greencatsoft.angularjs.core.{HttpPromise, HttpService}
+import com.greencatsoft.angularjs.core.{HttpPromise, HttpService, Promise}
 import com.greencatsoft.angularjs.{Factory, Service, injectable}
 import upickle.default.{Reader => UPReader, _}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.scalajs.js
-import scala.scalajs.js.JSON
+import scala.scalajs.js.{Function1, JSON}
 import scalaz.Scalaz._
 import scalaz._
+import scala.scalajs.js.Dynamic.{ global => g }
+
 /**
   * Created by alex on 17/02/16.
   */
 @injectable("ajax")
 class AjaxService(http: HttpService) extends Service {
 
-  def get[E](url: String)(implicit reader: UPReader[ValidationNel[String, E]]): Future[E] = connect(http.get[js.Any])(url)
-  def put[E](url: String)(implicit reader: UPReader[ValidationNel[String, E]]): Future[E] = connect(http.put[js.Any])(url)
+  def get[E](url: String)(implicit reader: UPReader[ValidationNel[String, E]]): Future[E] = connect(http.get[Response])(url)
+  def put[E](url: String)(implicit reader: UPReader[ValidationNel[String, E]]): Future[E] = connect(http.put[Response])(url)
 
-  private def connect[E](promiseFactory: String => HttpPromise[js.Any])(url: String)(implicit reader: UPReader[ValidationNel[String, E]]): Future[E] =
-    promiseFactory(url).map { response =>
-      val parseResponse = read[ValidationNel[String, E]](JSON.stringify(response))
+  private def connect[E](promiseFactory: String => Promise[Response])(url: String)(implicit reader: UPReader[ValidationNel[String, E]]): Future[E] = {
+    val p = scala.concurrent.Promise[Response]
+    promiseFactory(url).`then`(new Function1[Response, Response] {
+      override def apply(v: Response): Response = {
+        p.success(v)
+        v
+      }
+    })
+    p.future.map { response =>
+      val jsonResponse = JSON.stringify(response.data)
+      val parseResponse = read[ValidationNel[String, E]](jsonResponse)
       parseResponse.valueOr { msgs =>
         val msg = msgs.toList.mkString("\n")
         System.err.println(msg)
         throw new Exception(msg)
       }
     }
+  }
+}
+
+trait Response extends js.Object {
+  def data: js.Object = js.native
 }
 
 @injectable("ajax")
